dc13c2b2b8d70a5cac9dec07d78806f244a1bad3
nishantsinha24 <nishantsinha1024@gmail.com>
2023-08-01 15:28:55 +0530

    updated codes

---
+++ base/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Room
+# Create your views here.
+#rooms = [
+#    {'id' : 1, 'name' : 'lets learn Python !'},
+#    {'id' : 2, 'name': 'let us code in Java!'},
+#    {'id' : 3, 'name': 'we can do PHP as well'},
+#]
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room' : room}
+    return render(request, 'base/room.html', context)
+
+
