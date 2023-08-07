3755ba9f7a6de1428bed052799154a21e9127a40
nishantsinha24 <nishantsinha1024@gmail.com>
2023-08-07 14:35:40 +0530

    updated codes 2

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
 from .form import RoomForm
 # Create your views here.
@@ -23,7 +23,26 @@ def createRoom(request):
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
-    
-    
+            return redirect('home')
     context = {'form': form}
     return render(request, 'base/room_form.html' , context)
+
+def updateRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST , instance=room )
+        if form.is_valid(): 
+            form.save()
+            return redirect('home')
+    
+    context = {'form' : form}
+    return render(request, 'base/room_form.html' , context)
+
+def deleteRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html' , {'obj' : room})
